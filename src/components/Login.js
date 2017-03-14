96c83d86c8c1477b16f7a0bafdc7993aa413d12d
Chris Houdlette <choudlette@gmail.com>
2017-03-13 21:05:36 -0600

    facebook login is working

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import {Text, View, Image} from 'react-native';
 import styles from '../styles/style.js'
-// const FBSDK = require('react-native-fbsdk');
-// const {
-//   LoginButton,
-//   AccessToken
-// } = FBSDK;
+import FBSDK, {LoginButton, AccessToken} from 'react-native-fbsdk';
+
+
 
 export default class Login extends Component {
   render() {
@@ -15,37 +13,28 @@ export default class Login extends Component {
         source={require('../assets/images/SoccerBackground.jpg')}
         >
         <View style={styles.colorContainer}>
-
+          <View>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    alert(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => alert("logout.")}/>
+      </View>
         </View>
         </Image>
     )
   }
 }
-
-
-// var Login = React.createClass({
-//   render: function() {
-//     return (
-//       <View>
-//         <LoginButton
-//           publishPermissions={["publish_actions"]}
-//           onLoginFinished={
-//             (error, result) => {
-//               if (error) {
-//                 alert("login has error: " + result.error);
-//               } else if (result.isCancelled) {
-//                 alert("login is cancelled.");
-//               } else {
-//                 AccessToken.getCurrentAccessToken().then(
-//                   (data) => {
-//                     alert(data.accessToken.toString())
-//                   }
-//                 )
-//               }
-//             }
-//           }
-//           onLogoutFinished={() => alert("logout.")}/>
-//       </View>
-//     );
-//   }
-// });
