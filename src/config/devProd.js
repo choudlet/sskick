a1141df8c2fb1b8fe89dd42ea66a0f8878089acb
Chris Houdlette <choudlette@gmail.com>
2017-04-06 13:50:26 -0600

    devprod testing

--- src/config/devProd.js
+++ src/config/devProd.js
@@ -2,6 +2,6 @@ var serverPath;
 
 if (__DEV__) {
   serverPath = 'http://localhost:5000/'
-} else serverPath = 'http://localhost:5000/';
+} else serverPath = 'https://sskickserver.herokuapp.com/';
 
 export default serverPath;
